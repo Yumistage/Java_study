7e2e8b808569505ffa2fc4d166efe737cb2c3966
yumistage <235868521@qq.com>
2020-01-31 23:33:29 +0800

    1-31

--- src/test/java/com/zeroten/classObject/VarTest.java
+++ src/test/java/com/zeroten/classObject/VarTest.java
@@ -9,12 +9,22 @@ public class VarTest {
 
     public int index;
 
+    public VarTest() {
+    }
+
+    public void Print(){
+        System.out.println(String.format("当前创建第%d个实例", this.count));
+    }
+
+
     public static void main(String[] args) {
 
         VarTest v1 = new VarTest();
         v1.index = 1;
         VarTest.count++;
-        System.out.println(String.format("当前创建第%d个实例", VarTest.count));
+        v1.Print();
+
+
         VarTest v2 = new VarTest();
         v2.index = 2;
         VarTest.count++;
